6083b308922809f93dda69db165456abe6cbb488
Laura Pham <l.pham2311@gmail.com>
2023-06-19 20:51:04 +1000

    add spaces for menu, hero & sections

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
-
 function App() {
 	return (
 		<main>
-			<div className='hero'>
+			<div className='menu flex justify-between items-center p-4'>
+				<h1>code with laura</h1>
+				<div></div>
+			</div>
+			<div className='hero flex h-[80vh] items-center justify-around bg-orange-600 p-8 text-white'>
 				<div className='hero-text'>
-					<h1>I am Laura Pham</h1>
+					<h1 className='text-bold'>I am Laura Pham</h1>
 					<p>And I'm a software engineer</p>
 					<div className='social-icons'></div>
 				</div>
-				<div className='hero-image'></div>
+				<div className='hero-image h-inherit'></div>
 			</div>
+			<div className='section-projects h-[80vh] bg-blue-600'></div>
+			<div className='section-about h-[80vh] bg-white-600'></div>
+			<div className='section-reviews h-[80vh] bg-purple-600'></div>
+			<div className='section-contact h-[80vh] bg-pink-600'></div>
 		</main>
 	);
 }
