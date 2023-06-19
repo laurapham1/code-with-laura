86c3d37660a3d5067ddd1f7769c54a4b2fe8966f
Laura Pham <l.pham2311@gmail.com>
2023-06-19 20:13:25 +1000

    add boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main>
+			<div className='hero'>
+				<div className='hero-text'>
+					<h1>I am Laura Pham</h1>
+					<p>And I'm a software engineer</p>
+					<div className='social-icons'></div>
+				</div>
+				<div className='hero-image'></div>
+			</div>
+		</main>
+	);
 }
 
 export default App;
