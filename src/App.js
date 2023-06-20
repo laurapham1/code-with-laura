abd5e79cf41dbfa04360d5cfd5eaa70fbe78d4c1
Laura Pham <laurapham@Lauras-MacBook-Pro.local>
2023-06-20 20:25:31 +1000

    set active nav item on page scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './App.css';
 
 const navItems = [
@@ -9,13 +9,35 @@ const navItems = [
 	{title: 'Reviews', id: "reviews", className: "bg-pink-600"},
 ]
 const App = () => {
-	const [activeNavItem, setActiveNavItem] = useState("home")
+	const [activeNavItem, setActiveNavItem] = useState('home')
 	const handleClickNavItem = (e, item) => {
 		e.preventDefault()
 		setActiveNavItem(item.id)
 		const element = document.getElementById(item.id);
 		element.scrollIntoView(element);
 	}
+	// todo: update the active nav item as the page scrolls
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll, { passive: true });
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
+	const handleScroll = () => {
+		const activeNavPosition = document.getElementById(activeNavItem).getBoundingClientRect()		
+		if (activeNavPosition.height - Math.abs(activeNavPosition.top) < activeNavPosition.height/2 || activeNavPosition.height - Math.abs(activeNavPosition.bottom) < activeNavPosition.height/2) {
+			const sectionElements = document.querySelectorAll('.section')
+			const newActiveElement = Array.from(sectionElements).find((element) => {
+				const elementPosition = element.getBoundingClientRect()
+				return elementPosition.height - Math.abs(elementPosition.top) > elementPosition.height/2
+			})
+			if (newActiveElement?.id) {
+				setActiveNavItem(newActiveElement?.id)
+			}
+		}
+	};
+	
 	return (
 		<main>
 			<div className='menu flex justify-between items-center p-4' id="home">
@@ -23,22 +45,24 @@ const App = () => {
 				<div id="navbar" className="flex gap-4">
 					{navItems.map((item) => {
 						return (
-							<button onClick={(e) => handleClickNavItem(e, item)} className={activeNavItem === item.id ? 'text-bold border-b' : ''}>{item.title}</button>
-						)
-					})}
+								<button key={item.id} onClick={(e) => handleClickNavItem(e, item)} className={activeNavItem === item.id ? 'text-bold border-b' : ''}>{item.title}</button>
+							)
+						})}
+					</div>
 				</div>
-			</div>
-			<div className='hero flex h-[80vh] items-center justify-around bg-orange-600 p-8 text-white'>
-				<div className='hero-text'>
-					<h1 className='text-bold'>I am Laura Pham</h1>
-					<p>And I'm a software engineer</p>
-					<div className='social-icons'></div>
+			<div id="home" className="section">
+				<div className='hero flex h-[80vh] items-center justify-around bg-orange-600 p-8 text-white'>
+					<div className='hero-text'>
+						<h1 className='text-bold'>I am Laura Pham</h1>
+						<p>And I'm a software engineer</p>
+						<div className='social-icons'></div>
+					</div>
+					<div className='hero-image h-inherit'></div>
 				</div>
-				<div className='hero-image h-inherit'></div>
 			</div>
 			{navItems.filter((item) => item.id !== "home").map((item) => {
 				return (
-					<div id={item.id} className={`h-[80vh] p-4 ${item.className}`}>
+					<div key={item.id} id={item.id} className={`h-[80vh] p-4 ${item.className} section`}>
 						<h1 className="text-center">{item.title}</h1>
 					</div>
 				)
