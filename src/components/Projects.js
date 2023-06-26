2525b98744b3377020c4bbf6258a1cc8de405397
Laura Pham <l.pham2311@gmail.com>
2023-06-26 22:55:30 +1000

    change from flex to grid style

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,6 +11,10 @@ link: ''},
     description: 'An aeshetic daily to do list',
     image: '',
 link: ''},
+{title: 'Emotion Tracker', 
+    description: 'An emotion tracker/diary mobile app',
+    image: '',
+link: ''},
 ]
 
 const Projects = () => {	
@@ -20,12 +24,12 @@ const Projects = () => {
 	return (
         <>
         {/* <h1 className="text-center">Projects</h1> */}
-        <main className="p-10">
-            <div className="project-cards flex justify-center gap-4">
+        <main className="p-8 h-full">
+            <div className="project-cards grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 h-full">
                 {projects.map((project, index) => {
                     return (
                     <>
-                    <button className={`project-card relative h-[50vh] w-[50vh] bg-white shadow rounded-md flex items-end text-start text-black`} key={index} id={index} onClick={(e) => handleClickProject(e)}>
+                    <button className={`project-card bg-white shadow rounded-md flex items-end text-start text-black`} key={index} id={index} onClick={(e) => handleClickProject(e)}>
                         <div className="relative p-6">
                             <h1 className="text-light">{project.title}</h1>
                             <p>{project.description}</p>
