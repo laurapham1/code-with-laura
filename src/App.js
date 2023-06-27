6b0d248c6601af35b5b6e2f08923f475a36922af
Laura Pham <l.pham2311@gmail.com>
2023-06-27 21:00:03 +1000

    format files with prettier

--- src/App.js
+++ src/App.js
@@ -1,92 +1,148 @@
-import {useState, useEffect, useCallback} from 'react'
-import './App.css';
-import { FaBars, FaInstagram, FaLinkedin } from "react-icons/fa";
-import Projects from './components/Projects';
+import { useState, useEffect, useCallback } from 'react'
+import './App.css'
+import { FaBars, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import Projects from './components/Projects'
 
 const navItems = [
-	{title: 'Home', id: "home"},
-	{title: 'About', id: "about", contents: <p>Bringing keen attention to user experience to engineering</p>},
-	{title: 'Projects', id: "projects", className: "bg-cyan-600 text-white", contents: <Projects/>},
-	{title: 'Reviews', id: "reviews"},
-	{title: 'Contact', id: "contact", className: "bg-cyan-600 text-white"},
+    { title: 'Home', id: 'home' },
+    {
+        title: 'About',
+        id: 'about',
+        contents: (
+            <p>Bringing keen attention to user experience to engineering</p>
+        ),
+    },
+    {
+        title: 'Projects',
+        id: 'projects',
+        className: 'bg-cyan-600 text-white',
+        contents: <Projects />,
+    },
+    { title: 'Reviews', id: 'reviews' },
+    { title: 'Contact', id: 'contact', className: 'bg-cyan-600 text-white' },
 ]
 const App = () => {
-	const [activeNavItem, setActiveNavItem] = useState('home')
+    const [activeNavItem, setActiveNavItem] = useState('home')
 
-	const handleScroll = useCallback(() => {
-		const activeNavPosition = document.getElementById(activeNavItem).getBoundingClientRect()		
-		if (activeNavPosition.height - Math.abs(activeNavPosition.top) < activeNavPosition.height/2 || activeNavPosition.height - Math.abs(activeNavPosition.bottom) < activeNavPosition.height/2) {
-			const sectionElements = document.querySelectorAll('.section')
-			const newActiveElement = Array.from(sectionElements).find((element) => {
-				const elementPosition = element.getBoundingClientRect()
-				return elementPosition.height - Math.abs(elementPosition.top) > elementPosition.height/2
-			})
-			if (newActiveElement?.id) {
-				setActiveNavItem(newActiveElement?.id)
-			}
-		}
-	}, [activeNavItem]);
+    const handleScroll = useCallback(() => {
+        const activeNavPosition = document
+            .getElementById(activeNavItem)
+            .getBoundingClientRect()
+        if (
+            activeNavPosition.height - Math.abs(activeNavPosition.top) <
+                activeNavPosition.height / 2 ||
+            activeNavPosition.height - Math.abs(activeNavPosition.bottom) <
+                activeNavPosition.height / 2
+        ) {
+            const sectionElements = document.querySelectorAll('.section')
+            const newActiveElement = Array.from(sectionElements).find(
+                (element) => {
+                    const elementPosition = element.getBoundingClientRect()
+                    return (
+                        elementPosition.height - Math.abs(elementPosition.top) >
+                        elementPosition.height / 2
+                    )
+                }
+            )
+            if (newActiveElement?.id) {
+                setActiveNavItem(newActiveElement?.id)
+            }
+        }
+    }, [activeNavItem])
 
-	useEffect(() => {
-		window.addEventListener('scrollend', handleScroll, { passive: true });
-		return () => {
-			window.removeEventListener('scroll', handleScroll);
-		};
-	}, [handleScroll]);
+    useEffect(() => {
+        window.addEventListener('scrollend', handleScroll, { passive: true })
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [handleScroll])
 
+    const handleClickNavItem = (e, item) => {
+        e.preventDefault()
+        setActiveNavItem(item.id)
+        const element = document.getElementById(item.id)
+        const navElement = document.getElementById('navbar')
+        const newTop =
+            window.scrollY +
+            element.getBoundingClientRect().top -
+            navElement.getBoundingClientRect().bottom
+        window.scrollTo({ top: newTop, behavior: 'smooth' })
+    }
 
-	const handleClickNavItem = (e, item) => {
-		e.preventDefault()
-		setActiveNavItem(item.id)
-		const element = document.getElementById(item.id);
-		const navElement = document.getElementById('navbar')
-		const newTop = window.scrollY + element.getBoundingClientRect().top - navElement.getBoundingClientRect().bottom 
-		window.scrollTo({top:newTop, behavior: 'smooth'} )
-	}
-	
-	return (
-		<main className="">
-			<div id="navbar" className='menu flex justify-between items-center p-4 fixed top-0 bg-white/95 z-10 rounded-md m-2 w-[-webkit-fill-available] shadow'>
-				<h1 className="font-bold tracking-tight">LAURA PHAM</h1>
-						<div className="block md:hidden"><button><FaBars/></button></div>
-						<div className="gap-4 items-center hidden md:flex font-light">
-						{navItems.map((item) => {
-							const isActiveNav = activeNavItem === item.id
-							return (
-								<span class={isActiveNav ? `before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-cyan-600 relative inline-block` : ''}>
-									<button key={item.id} onClick={(e) => handleClickNavItem(e, item)} className={isActiveNav ? `relative text-white` : ""}>{item.title}</button>
-								</span>
-								
-							)
-						})}
-						</div>
-				</div>
-			<div id="home" className="section">
-				<div className='hero flex h-[85vh] items-center justify-around bg-cyan-600 p-8 text-white'>
-					<div className='hero-text flex flex-col gap-4'>
-						<span className="text-xl">
-							<h1 className=''>Hi, I'm Laura 👋</h1>
-							<p>Educator turned Software engineer</p>
-						</span>
-						<div className='social-icons flex flex-row gap-2'>
-							<button className="hover:scale-125 ease-in-out duration-300"><FaInstagram/></button>
-							<button className="hover:scale-125 ease-in-out duration-300"><FaLinkedin/></button>
-							
-						</div>
-					</div>
-					<div className='hero-image h-inherit'></div>
-				</div>
-			</div>
-			{navItems.filter((item) => item.id !== "home").map((item) => {
-				return (
-					<div key={item.id} id={item.id} className={`h-[85vh] p-4 ${item.className} section`}>
-						<h1 className="text-center font-light tracking-tight text-lg">{item.title}</h1>
-						{item.contents}
-					</div>
-				)
-			})}
-		</main>
-	);
+    return (
+        <main className="">
+            <div
+                id="navbar"
+                className="menu flex justify-between items-center p-4 fixed top-0 bg-white/95 z-10 rounded-md m-2 w-[-webkit-fill-available] shadow"
+            >
+                <h1 className="font-bold tracking-tight">LAURA PHAM</h1>
+                <div className="block md:hidden">
+                    <button>
+                        <FaBars />
+                    </button>
+                </div>
+                <div className="gap-4 items-center hidden md:flex font-light">
+                    {navItems.map((item) => {
+                        const isActiveNav = activeNavItem === item.id
+                        return (
+                            <span
+                                class={
+                                    isActiveNav
+                                        ? `before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-cyan-600 relative inline-block`
+                                        : ''
+                                }
+                            >
+                                <button
+                                    key={item.id}
+                                    onClick={(e) => handleClickNavItem(e, item)}
+                                    className={
+                                        isActiveNav ? `relative text-white` : ''
+                                    }
+                                >
+                                    {item.title}
+                                </button>
+                            </span>
+                        )
+                    })}
+                </div>
+            </div>
+            <div id="home" className="section">
+                <div className="hero flex h-[85vh] items-center justify-around bg-cyan-600 p-8 text-white">
+                    <div className="hero-text flex flex-col gap-4">
+                        <span className="text-xl">
+                            <h1 className="">Hi, I'm Laura 👋</h1>
+                            <p>Educator turned Software engineer</p>
+                        </span>
+                        <div className="social-icons flex flex-row gap-2">
+                            <button className="hover:scale-125 ease-in-out duration-300">
+                                <FaInstagram />
+                            </button>
+                            <button className="hover:scale-125 ease-in-out duration-300">
+                                <FaLinkedin />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="hero-image h-inherit"></div>
+                </div>
+            </div>
+            {navItems
+                .filter((item) => item.id !== 'home')
+                .map((item) => {
+                    return (
+                        <div
+                            key={item.id}
+                            id={item.id}
+                            className={`h-[85vh] p-4 ${item.className} section`}
+                        >
+                            <h1 className="text-center font-bold text-xl">
+                                {item.title}
+                            </h1>
+                            {item.contents}
+                        </div>
+                    )
+                })}
+        </main>
+    )
 }
 
-export default App;
+export default App
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,48 +1,62 @@
 const projects = [
-    {title: 'Sammi', 
-    description: 'A mental health support app for teachers & students',
-    image: '',
-link: ''},
-{title: 'Furreal', 
-    description: 'A pet rental service app',
-    image: '',
-link: ''},
-{title: 'Fun to do list', 
-    description: 'An aeshetic daily to do list',
-    image: '',
-link: ''},
-{title: 'Emotion Tracker', 
-    description: 'An emotion tracker/diary mobile app',
-    image: '',
-link: ''},
+    {
+        title: 'Sammi',
+        description: 'A mental health support app for teachers & students',
+        image: '',
+        link: '',
+    },
+    {
+        title: 'Furreal',
+        description: 'A pet rental service app',
+        image: '',
+        link: '',
+    },
+    {
+        title: 'Fun to do list',
+        description: 'An aeshetic daily to do list',
+        image: '',
+        link: '',
+    },
+    {
+        title: 'Emotion Tracker',
+        description: 'An emotion tracker/diary mobile app',
+        image: '',
+        link: '',
+    },
 ]
 
-const Projects = () => {	
+const Projects = () => {
     const handleClickProject = (e) => {
         console.log(e)
     }
-	return (
+    return (
         <>
-        {/* <h1 className="text-center">Projects</h1> */}
-        <main className="p-8 h-full">
-            <div className="project-cards grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 h-full">
-                {projects.map((project, index) => {
-                    return (
-                    <>
-                    <button className={`project-card bg-white shadow rounded-md flex items-end text-start text-black`} key={index} id={index} onClick={(e) => handleClickProject(e)}>
-                        <div className="relative p-6">
-                            <h1 className="text-light">{project.title}</h1>
-                            <p>{project.description}</p>
-                        </div>
-                    </button>
-                    </>
-                )})}
-            </div>
-            
-        </main>
+            {/* <h1 className="text-center">Projects</h1> */}
+            <main className="p-8 h-full">
+                <div className="project-cards grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 h-full">
+                    {projects.map((project, index) => {
+                        return (
+                            <>
+                                <button
+                                    className={`project-card bg-white shadow rounded-md flex items-end text-start text-black`}
+                                    key={index}
+                                    id={index}
+                                    onClick={(e) => handleClickProject(e)}
+                                >
+                                    <div className="relative p-6">
+                                        <h1 className="text-light">
+                                            {project.title}
+                                        </h1>
+                                        <p>{project.description}</p>
+                                    </div>
+                                </button>
+                            </>
+                        )
+                    })}
+                </div>
+            </main>
         </>
-        
     )
 }
 
-export default Projects;
+export default Projects
