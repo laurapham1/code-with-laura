d948c25928a2552a2cf9d6bd92893140b2ce02bc
Laura Pham <l.pham2311@gmail.com>
2023-07-01 17:42:53 +1000

    add typing animation to banner heading

--- src/App.js
+++ src/App.js
@@ -123,10 +123,14 @@ const App = () => {
             <div id="home" className="section">
                 <div className="hero flex h-[85vh] items-center justify-around bg-sky-100 p-8">
                     <div className="hero-text flex flex-col gap-4">
-                        <span className="text-xl font-light">
+                        <div className="text-2xl font-light w-fit">
+                            <div className="typewriter ">
                             <h1 className="">Hi, I'm Laura 👋</h1>
-                            <p>Educator turned Software engineer</p>
-                        </span>
+                            </div>
+                        </div>
+                        <div className="text-2xl font-light w-fit">
+                            <h2 className="">Educator turned Software engineer</h2>
+                        </div>
                         <div className="social-icons flex flex-row gap-2">
                             <button className="hover:scale-125 ease-in-out duration-300">
                                 <FaInstagram />
