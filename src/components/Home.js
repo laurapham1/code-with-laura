265c5210e7a4367c5db949eb0e8f93e6a4442d07
Laura Pham <l.pham2311@gmail.com>
2023-07-17 23:14:12 +1000

    move banner text closer to center

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import profileImage from '../assets/images/profile.png'
 const Home = () => {
     return (
         <div className="hero grid grid-cols-1 md:grid-cols-2 pt-[10vh] h-fit min-h-fit items-center justify-around bg-purple-800 text-white">
-            <div className="hero-text flex flex-col gap-4 p-8">
+            <div className="hero-text flex flex-col gap-4 p-8 ml-0 md:ml-[25%]">
                 <div className="text-3xl md:text-5xl font-bold w-fit">
                     <div className="typewriter ">
                         <h1 className="">Hi, I'm Laura 👋</h1>
