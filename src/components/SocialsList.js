78f32cae4b9d291fc639dfa9cbeffd0318ddb89a
Laura Pham <l.pham2311@gmail.com>
2023-07-19 20:45:01 +1000

    update socials list styles

--- src/components/SocialsList.js
+++ src/components/SocialsList.js
@@ -3,36 +3,36 @@ import { FaTree, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa'
 const SocialsList = () => {
     return (
         <div className="social-icons flex flex-row gap-2">
-            <button className="hover:scale-125 ease-in-out duration-300">
+            <button className="hover:scale-125 ease-in-out duration-300 p-2 border rounded-[50%]">
                 <a
                     href="https://www.github.com/laurapham1"
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <FaGithub />
+                    <FaGithub className="text-lg"/>
                 </a>
             </button>
-            <button className="hover:scale-125 ease-in-out duration-300">
+            <button className="hover:scale-125 ease-in-out duration-300 p-2 border rounded-[50%]">
                 <a
                     href="https://www.linkedin.com/in/lauranpham"
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <FaLinkedin />
+                    <FaLinkedin className="text-lg"/>
                 </a>
             </button>
-            <button className="hover:scale-125 ease-in-out duration-300">
+            <button className="hover:scale-125 ease-in-out duration-300 p-2 border rounded-[50%]">
                 <a href="https://www.linktr.ee/codewithlaura" target="_blank" rel="noreferrer">
-                    <FaTree />
+                    <FaTree className="text-lg"/>
                 </a>
             </button>
-            <button className="hover:scale-125 ease-in-out duration-300">
+            <button className="hover:scale-125 ease-in-out duration-300 p-2 border rounded-[50%]">
                 <a
                     href="https://www.instagram.com/lauracodes_/"
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <FaInstagram />
+                    <FaInstagram className="text-lg"/>
                 </a>
             </button>
         </div>
