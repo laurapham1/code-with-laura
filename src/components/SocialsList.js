2405f89b3e52231ced17f377c4e4e41a17ec6cb5
Laura Pham <l.pham2311@gmail.com>
2023-07-19 20:28:23 +1000

    add github to socials list

--- src/components/SocialsList.js
+++ src/components/SocialsList.js
@@ -1,15 +1,15 @@
-import { FaTree, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import { FaTree, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa'
 
 const SocialsList = () => {
     return (
         <div className="social-icons flex flex-row gap-2">
             <button className="hover:scale-125 ease-in-out duration-300">
                 <a
-                    href="https://www.instagram.com/lauracodes_/"
+                    href="https://www.github.com/laurapham1"
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <FaInstagram />
+                    <FaGithub />
                 </a>
             </button>
             <button className="hover:scale-125 ease-in-out duration-300">
@@ -24,6 +24,15 @@ const SocialsList = () => {
             <button className="hover:scale-125 ease-in-out duration-300">
                 <a href="https://www.linktr.ee/codewithlaura" target="_blank" rel="noreferrer">
                     <FaTree />
+                </a>
+            </button>
+            <button className="hover:scale-125 ease-in-out duration-300">
+                <a
+                    href="https://www.instagram.com/lauracodes_/"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FaInstagram />
                 </a>
             </button>
         </div>
